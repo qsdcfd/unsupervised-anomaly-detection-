d59d62fe3b66419cb25d862f9e0c70dfec1e7f49
이세현 <86671456+qsdcfd@users.noreply.github.com>
2022-07-14 13:07:50 +0900

    Add files via upload

---
+++ Models/DL/Preprocessing/AutoEncoder/Trainer/Prediction/best_model/Parameters.py
@@ -0,0 +1,23 @@
+import random
+import pandas as pd
+import numpy as np
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+from tqdm.auto import tqdm
+from sklearn.metrics import f1_score
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+def seed_everything(seed):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+
